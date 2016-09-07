38b60221fab2368ee16764e3cf3b24861eaefb65
Eric Freda <eric.freda@yahoo.com>
2016-09-07 05:00:50 +0000

    fixed merging error with helloworld.sh

--- helloworld.sh
+++ helloworld.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-<<<<<<< HEAD
+
 # A helloworld script
-# Eric Freda 9/6/2016
+# Eric Freda and Kaiqin Huang 9/6/2016
 
 echo Hello world.
-=======
-echo Hello World
-
->>>>>>> d5556243dc729dce73aa25e2be36815c40af450f
