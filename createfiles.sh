aae492bc0fc2574fa3c4bee71b5a065aaefb25fe
Eric Freda <eric.freda@yahoo.com>
2016-09-13 02:09:36 +0000

    finished createfiles.sh

--- createfiles.sh
+++ createfiles.sh
@@ -1,26 +1,11 @@
 #!/bin/bash
-# A script to create a directory, fill it with files, then append to those files
-# Eric Freda 9/6/2016
 
 mkdir tmpfiles
 
-count=1
-
-while [ $count -le 9 ]
-do
-	touch ./tmpfiles/file00$count.tmp
-	let count++
+for i in $(seq -w 100); do
+	touch ./tmpfiles/file$i.tmp
 done
 
-while [ $count -le 99 ]
-do
-	touch ./tmpfiles/file0$count.tmp
-	let count++
+for i in $(seq -w 100); do
+	echo "Temporary file " $i >> ./tmpfiles/file$i.tmp
 done
-
-touch ./tmpfiles/file$count.tmp
-
-echo The contents of ./tmpfiles are:
-ls ./tmpfiles
-
-
