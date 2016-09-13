28835c741de2492eab027fde1d86e86587f9de88
Eric Freda <eric.freda@yahoo.com>
2016-09-13 02:17:44 +0000

    modified test_bash.py

--- test_bash.py
+++ test_bash.py
@@ -1,7 +1,7 @@
 import subprocess as sp
 
 def test_helloworld():
-	assert sp.check_output("./helloworld.sh") == "Hello world."
+	assert sp.check_output("./helloworld.sh") == "Hello world.\n"
 
 def test_countup():
-	assert sp.check_output(["./countup.sh","5"]) == "1 2 3 4 5/n"
+	assert sp.check_output(["./countup.sh","5"]) == "1 2 3 4 5\n"
